1c34493dfc80fcb5a0f27a941aaff12fe830cc51
Donald Davies <40407408+Dcdavies243@users.noreply.github.com>
2018-09-05 14:44:29 +0100

    Added proper grammer

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,17 @@
 //include libraries
 #include <stdlib.h>
-
+#include <iostream>
 
 
 //Entry point for our program
 //Every C++ program needs a main() function
 int main()
 {
+	std::cout << "Hello world!" << std::endl;
+	std::cout << std::endl;
+
 	//Wait for user to press button before continuing
 	system("PAUSE");
-
 
 	//Tell the operating system we are done
 	//and there was no error
